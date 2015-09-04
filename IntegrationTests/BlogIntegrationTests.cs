9e4f381bd4ad8b11a393a58b2909c5f44a328b20
Ilya Ivanov <static.ila@gmail.com>
2015-09-04 09:58:06 +0300

    Initial commit

---
+++ IntegrationTests/BlogIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using NDbUnit_Sample;
+using NUnit.Framework;
+
+namespace IntegrationTests
+{
+    public class BlogIntegrationTests
+    {
+        [Test]
+        public void SavingABlogShouldSaveIt()
+        {
+            var context = new BlogContext();
+
+            var newBlog = new Blog()
+            {
+                Name = "My Blog"
+            };
+            context.Blogs.Add(newBlog);
+            context.SaveChanges();
+            context.Dispose();
+
+            context = new BlogContext();
+            context.Database.Log = logString => Console.WriteLine(logString);
+            context.Blogs.First().Name.Should().Be("My Blog");
+        }
+
+        
+    }
+}
